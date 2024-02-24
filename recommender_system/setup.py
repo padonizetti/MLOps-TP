8e112aa7c4923a4355fb0b913351f4b3dd8c4fac
Paolo Donizetti <padonizetti@gmail.com>
2024-02-24 15:45:15 -0300

    full pipeline working commit

--- recommender_system/setup.py
+++ recommender_system/setup.py
@@ -1,16 +1,15 @@
 from setuptools import find_packages, setup
 import os
-DAGSTER_VERSION=os.getenv('DAGSTER_VERSION', '1.6.4')
-DAGSTER_LIBS_VERSION=os.getenv('DAGSTER_LIBS_VERSION', '0.22.4')
+DAGSTER_VERSION=os.getenv('DAGSTER_VERSION', '1.6.3')
+DAGSTER_LIBS_VERSION=os.getenv('DAGSTER_LIBS_VERSION', '0.22.3')
 MLFLOW_VERSION=os.getenv('MLFLOW_VERSION', '2.10.0')
 
 setup(
     name="recommender_system",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["recommender_system_tests"]),
     install_requires=[
         f"dagster=={DAGSTER_VERSION}",
-        f"dagit=={DAGSTER_VERSION}",
         "dagster-dbt",
         "dbt-postgres",
         f"dagster-mlflow=={DAGSTER_LIBS_VERSION}",
---
+++ recommender_system/testing_notebook.py
@@ -0,0 +1,44 @@
+# %%
+import mlflow
+logged_model = 'models:/keras_dot_product_model/1'
+
+# Load model as a PyFuncModel.
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+# %%
+
+# %%
+# Predict on a Pandas DataFrame.
+import numpy as np
+loaded_model.predict([np.array([1, 2]), np.array([2, 3])])
+# %%
+# %%
+mlflow.set_experiment('test')
+# %%
+# %%
+mlflow.start_run()
+# %%
+from tensorflow.keras import layers, Model
+# %%
+input = layers.Input(shape=(1,))
+dense = layers.Dense(10)(input)
+model = Model(input, dense)
+# %%
+model.summary()
+# %%
+logged_model = mlflow.tensorflow.log_model(
+        model,
+        "simple",
+        registered_model_name='simple'
+    )
+# %%
+logged_model.__dir__()
+# %%
+
+# %%
+{
+    'model_uri': logged_model.model_uri,
+    'run_id': logged_model.run_id
+}
+# %%
+logged_model.flavors
+# %%
